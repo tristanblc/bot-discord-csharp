3b272a3d1665c249aaafa992fba7e26e1be6c186
tristanblc <tblanc.sio@gmail.com>
2022-03-31 16:27:28 +0200

    add service for shape & stoptimes

---
+++ My-Bot-Discord-CSharp/ReaderClassLibrary/Services/ShapeService.cs
@@ -0,0 +1,21 @@
+﻿using ApiApplication.Repository.Interface;
+using BusClassLibrary;
+using ReaderClassLibrary.Interfaces;
+using ReaderClassLibrary.Reader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReaderClassLibrary.Services
+{
+    public class ShapeService : GenericApiReader<Shape>, IBusInfoService<Shape>
+    {
+
+        public ShapeService(HttpClient httpClient, string uri) : base(httpClient, uri)
+        {
+
+        }
+    }
+}
