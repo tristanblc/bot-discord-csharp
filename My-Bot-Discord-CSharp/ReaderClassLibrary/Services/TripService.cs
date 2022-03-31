ceb6071775346c8ed6959ec5d7436fad0e8ce696
tristanblc <tblanc.sio@gmail.com>
2022-03-31 17:24:04 +0200

    add trip service

---
+++ My-Bot-Discord-CSharp/ReaderClassLibrary/Services/TripService.cs
@@ -0,0 +1,19 @@
+﻿using BusClassLibrary;
+using ReaderClassLibrary.Interfaces;
+using ReaderClassLibrary.Reader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReaderClassLibrary.Services
+{
+    public class TripService :  GenericApiReader<Trip>, IBusInfoService<Trip>
+    {
+        public TripService(HttpClient httpClient, string uri) : base(httpClient, uri)
+        {
+
+        }
+    }
+}
