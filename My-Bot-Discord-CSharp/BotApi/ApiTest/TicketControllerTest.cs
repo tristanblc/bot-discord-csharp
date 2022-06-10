4c31d278bd1b114f9e1ec21a3b20d6375901611a
tristanblc <tblanc.sio@gmail.com>
2022-06-10 14:44:35 +0200

    add test

---
+++ My-Bot-Discord-CSharp/BotApi/ApiTest/TicketControllerTest.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReaderClassLibrary.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotApi.ApiTest
+{
+    [TestClass]
+    public class TicketControllerTest
+    {
+        private readonly HttpClient HttpClient = new HttpClient();
+
+        private readonly string Url;
+        private TicketService TicketService { get; set; }
+        public TicketControllerTest()
+        {
+
+            var builder = new ConfigurationBuilder()
+                                .AddJsonFile("app.json", optional: false, reloadOnChange: true)
+                                .AddEnvironmentVariables();
+
+            IConfiguration config = builder.Build();
+
+            Url = config.GetSection("apiUrl").Value.ToString() + "/Ticket";
+
+            TicketService = new TicketService(HttpClient,Url);
+                
+        }
+
+        [TestMethod]
+        public void GetAllTicketsTest()
+        {
+
+
+        }
+
+        [TestMethod]
+        public void GetTicketTest()
+        {
+
+
+
+        }
+
+
+        [TestMethod]
+        public void AddTicketTest()
+        {
+
+        }
+
+
+        [TestMethod]
+        public void UpdateTicketTest()
+        {
+
+
+        }
+
+
+        [TestMethod]
+        public void DeleteTicketMethod()
+        {
+
+        }
+    }
+}
