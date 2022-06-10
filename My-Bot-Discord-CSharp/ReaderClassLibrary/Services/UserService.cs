9bc6f950c2d0e14256baba3e565c8359f67a3d71
tristanblc <tblanc.sio@gmail.com>
2022-06-10 21:28:35 +0200

    change user service

---
+++ My-Bot-Discord-CSharp/ReaderClassLibrary/Services/UserService.cs
@@ -0,0 +1,18 @@
+﻿using BotClassLibrary;
+using ReaderClassLibrary.Interfaces;
+using ReaderClassLibrary.Reader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReaderClassLibrary.Services
+{
+    public class UserService : GenericApiReader<Users>, IGenericInterface<Users>
+    {           
+        public UserService(HttpClient httpClient, string baseuri) : base(httpClient, baseuri)
+        {
+        }
+    }
+}
