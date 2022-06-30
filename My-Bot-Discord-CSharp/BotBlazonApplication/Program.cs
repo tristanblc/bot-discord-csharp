1ac339adeede34fbc6f1849fa85c435a7f9e0b52
tristanblc <tblanc.sio@gmail.com>
2022-06-30 17:26:30 +0200

    add front loggin pages

--- My-Bot-Discord-CSharp/BotBlazonApplication/Program.cs
+++ My-Bot-Discord-CSharp/BotBlazonApplication/Program.cs
@@ -1,11 +1,13 @@
 using BotBlazonApplication;
+using BotBlazonApplication.Services.Interface;
+using BotBlazonApplication.Services.Service;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
+builder.Services.AddSingleton<ILocalStorage, LocalStorage>(); // NOTE: this line is newly added
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
---
+++ My-Bot-Discord-CSharp/BotBlazonApplication/Services/Classes/Compressor.cs
@@ -0,0 +1,34 @@
+﻿using System.IO.Compression;
+
+namespace BotBlazonApplication.Services.Classes
+{
+	internal class Compressor
+	{
+		public static async Task<byte[]> CompressBytesAsync(byte[] bytes)
+		{
+			using (var outputStream = new MemoryStream())
+			{
+				using (var compressionStream = new GZipStream(outputStream, CompressionLevel.Optimal))
+				{
+					await compressionStream.WriteAsync(bytes, 0, bytes.Length);
+				}
+				return outputStream.ToArray();
+			}
+		}
+
+		public static async Task<byte[]> DecompressBytesAsync(byte[] bytes)
+		{
+			using (var inputStream = new MemoryStream(bytes))
+			{
+				using (var outputStream = new MemoryStream())
+				{
+					using (var compressionStream = new GZipStream(inputStream, CompressionMode.Decompress))
+					{
+						await compressionStream.CopyToAsync(outputStream);
+					}
+					return outputStream.ToArray();
+				}
+			}
+		}
+	}
+}
